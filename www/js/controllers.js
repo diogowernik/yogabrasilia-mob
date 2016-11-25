f34e07cec12cf03249e33b345c3013ff356c1ca8
brasiliaio-mob <diogo.carvalho@tjdft.jus.br>
2016-11-25 11:19:39 -0500

    remove duplicate controller
    Signed-off-by: brasiliaio-mob <diogo.carvalho@tjdft.jus.br>

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -271,44 +271,6 @@ angular.module('starter.controllers', [])
     ionicMaterialInk.displayEffect();
 })
 
-.controller('PublicationsCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk, $http) {
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.isExpanded = false;
-    $scope.$parent.setExpanded(false);
-    $scope.$parent.setHeaderFab('right');
-
-    $http({
-      method: 'GET',
-      url: 'http://yoga.smartmarket.io/publications/from-subdomain/1.json'
-  }).then(function successCallback(response) {
-    $scope.users = response.data;
-    
-    $http({
-      method: 'GET',
-      url: 'http://yoga.smartmarket.io/publications/from-subdomain/1.json'
-  }).then(function successCallback(response) {                
-    $scope.publications = response.data;                                
-    $timeout(function() {
-        ionicMaterialMotion.fadeSlideIn({
-            selector: '.animate-fade-slide-in .item'
-        });
-    }, 200);
-    
-}, function errorCallback(response) {
-    $scope.error = response;
-});
-}, function errorCallback(response) {
-    $scope.error = response;
-});
-
-
-
-
-    // Activate ink for controller
-    ionicMaterialInk.displayEffect();
-})
-
 .controller('CardsCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk, $http) {
     $scope.$parent.showHeader();
     $scope.$parent.clearFabs();
