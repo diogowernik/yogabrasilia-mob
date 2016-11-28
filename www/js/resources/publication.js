355e04f3a94b7df8e84d773a0261c4039acf6bc4
brasiliaio-mob <diogo.carvalho@tjdft.jus.br>
2016-11-28 15:30:13 -0500

    fix resource to backend model
    Signed-off-by: brasiliaio-mob <diogo.carvalho@tjdft.jus.br>

--- www/js/resources/publication.js
+++ www/js/resources/publication.js
@@ -1,3 +1,8 @@
+/* global angular, document, window , urlBase, subDomainId*/
+'use strict';
 angular.module('starter').factory('Publication', function($resource) {
-  return $resource(rootURL + 'publications/from-subdomain/' + SubdomainId +'.json'); 
+  return $resource(urlBase + 'publications/:action/:id.json', { id: '@id'}, {
+          update: { method: 'PUT' },
+          fromSubDomain: { isArray: true, params:{action:'from-subdomain', id: subDomainId}},
+        });
 });
